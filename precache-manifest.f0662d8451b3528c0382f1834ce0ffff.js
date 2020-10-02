7242b0d3c6a29ac6e216774b3accf515353dc66c
alanli2001 <alanli2001@users.noreply.github.com>
2020-10-02 16:44:19 -0400

    Updates

---
+++ precache-manifest.f0662d8451b3528c0382f1834ce0ffff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bd6400d44c3076e037a6143c85602a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "52a92c767cf859cc7d88",
+    "url": "/home/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a92c767cf859cc7d88",
+    "url": "/home/static/js/main.aaad413b.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "43c3bcd5056ed5998bf6e575fc608434",
+    "url": "/home/static/media/resume.43c3bcd5.pdf"
+  }
+]);
